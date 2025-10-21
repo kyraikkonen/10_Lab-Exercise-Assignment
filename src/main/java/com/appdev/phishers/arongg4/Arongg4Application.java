f9bd7ec67387338ccb29e6d70f75a76dd28756d7
Ky <kylenearong131@gmail.com>
2025-10-21 18:21:52 +0800

    initial project setup

---
+++ src/main/java/com/appdev/phishers/arongg4/Arongg4Application.java
@@ -0,0 +1,13 @@
+package com.appdev.phishers.arongg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arongg4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arongg4Application.class, args);
+	}
+
+}
